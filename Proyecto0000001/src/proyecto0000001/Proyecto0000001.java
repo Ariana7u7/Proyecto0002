58416d9777ebee586700afde618d84e7db17c4e2
lab-205 <lab-205@LP-10-205.iberotec.edu.pe>
2018-01-25 15:48:51 -0500

    XDDDD

---
+++ Proyecto0000001/src/proyecto0000001/Proyecto0000001.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto0000001;
+
+/**
+ *
+ * @author lab-205
+ */
+public class Proyecto0000001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("FGH NEWQT");
+    }
+    
+}
